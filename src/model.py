639ec2baedaa59e31592fe29c5ea02ec0e703dfb
Obata Yutaka <obata0904@outlook.jp>
2025-11-10 15:35:43 +0900

    feat: add physics-based loss function

--- src/model.py
+++ src/model.py
@@ -21,19 +21,3 @@ class NN(nn.Module):
         kappa = -F.softplus(out[:, 1])
         chi_n = -F.softplus(out[:, 2])
         return torch.stack([eps, kappa, chi_n], dim=1)
-
-
-
-def IBM2_PES(params, n_pi, n_nu, beta_f):
-    beta_b = beta_f*5.0
-
-    beta2 = beta_b**2
-    A = 1/(1 + beta2)
-    SQRT_2_OVER_7 = math.sqrt(2.0/7.0)
-
-    term_1 = params[:, 0]*(n_pi + n_nu)*beta2*A
-    term_2 = n_pi*n_nu*params[:, 1]*beta2*A*A*(
-        4 - 2*SQRT_2_OVER_7*(params[:, 2] - 0.5)*beta_b - params[:, 2]*beta2/7
-    )
-
-    return term_1 + term_2
