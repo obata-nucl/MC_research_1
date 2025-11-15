2b01d23a1bbedf147e384bfde98ce4983b49f678
Obata Yutaka <obata0904@outlook.jp>
2025-11-16 00:48:20 +0900

    feat(model/train): change IBM2_PES to accept beta tensor

--- src/physics.py
+++ src/physics.py
@@ -3,15 +3,18 @@ import torch
 
 def IBM2_PES(params: torch.Tensor, n_pi: torch.Tensor, n_nu: torch.Tensor, beta_f: torch.Tensor) -> torch.Tensor:
     """ Compute IBM2 potential energy surface (PES) """
+    eps = params[:, 0].unsqueeze(1)
+    kappa = params[:, 1].unsqueeze(1)
+    chi_n = params[:, 2].unsqueeze(1)
     beta_b = beta_f*5.0
 
     beta2 = beta_b**2
     A = 1/(1 + beta2)
-    SQRT_2_OVER_7 = math.sqrt(2.0/7.0)
+    SQRT_2_OVER_7 = torch.tensor(math.sqrt(2.0/7.0), dtype=params.dtype, device=params.device)
 
-    term_1 = params[:, 0]*(n_pi + n_nu)*beta2*A
-    term_2 = n_pi*n_nu*params[:, 1]*beta2*A*A*(
-        4 - 2*SQRT_2_OVER_7*(params[:, 2] - 0.5)*beta_b - params[:, 2]*beta2/7
+    term_1 = eps*(n_pi + n_nu)*beta2*A
+    term_2 = n_pi*n_nu*kappa*beta2*A*A*(
+        4.0 - 2.0*SQRT_2_OVER_7*(chi_n - 0.5)*beta_b - chi_n*beta2/7.0
     )
 
     return term_1 + term_2
