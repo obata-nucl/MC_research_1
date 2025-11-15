882232166af5dc98ce3a2b478e21518deda6a933
Obata Yutaka <obata0904@outlook.jp>
2025-11-15 18:29:39 +0900

    feat(loader): add function to load evaluation results

--- src/loader.py
+++ src/loader.py
@@ -26,3 +26,14 @@ def load_eval_summary(top_k: int = 5) -> list[list[int]]:
             if len(patterns) >= top_k:
                 break
     return patterns
+
+def load_eval_results() -> dict[str, np.ndarray]:
+    """ load evaluation results(pattern, spectra, ratio, parameters) """
+    load_dir = CONFIG["paths"]["results_dir"] / "evaluation"
+    results = {}
+    for file_name in load_dir.iterdir():
+        if file_name.suffix == ".csv" and file_name.name != "eval_summary.csv":
+            stem = file_name.stem
+            data = np.loadtxt(file_name, delimiter=',', skiprows=1)
+            results[stem] = data
+    return results
