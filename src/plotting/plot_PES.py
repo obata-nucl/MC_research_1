2b92050480bac0387a3fd09a6138b56812a9f9f4
Obata Yutaka <obata0904@outlook.jp>
2025-11-16 01:30:33 +0900

    feat(plot): add plot_n_PES function fot plotting multiple PES

--- src/plotting/plot_PES.py
+++ src/plotting/plot_PES.py
@@ -31,9 +31,20 @@ def plot_n_PES(ax: plt.Axes, N: int, n_pi: int, n_nu: int, beta_f_arr: np.ndarra
     pred_PES = calc_PES(params, 6, n_nu, beta_f_arr)
     ax.plot(beta_f_arr, pred_PES, linestyle='-', color="black", label="IBM PES")
 
-    min_idx = np.argmin(pred_PES)
-    ax.plot(beta_f_arr[min_idx], pred_PES[min_idx], marker='ro', markersize=6)
-    ax.plot(expt_PES[:, 0])
+    idx_min_calc = np.argmin(pred_PES)
+    ax.plot(beta_f_arr[idx_min_calc], pred_PES[idx_min_calc], marker='ro', markersize=6)
+
+    expt_PES = expt_PES.get((62, N))
+    ax.plot(expt_PES[:, 0], expt_PES[:, 1], linestyle="--", color="tab:orange", label="HFB PES")
+    idx_min_expt = np.argmin(expt_PES[:, 1])
+    ax.plot(expt_PES[idx_min_expt, 0], expt_PES[idx_min_expt, 1], marker='ro', markersize=6)
+
+    ax.set_title(rf"$^{62 + {N}}Sm$", fontsize=18)
+    ax.set_xlabel(r"$\beta$", fontsize=14)
+    ax.set_ylabel("Energy [MeV]", fontsize=14)
+    ax.tick_params(axis="both", which="major", labelsize=12)
+    ax.legend(loc="best", fontsize=12)
+    return ax
 
 def main():
     beta_f_arr = np.arange(-0.45, 0.61, 0.01)
