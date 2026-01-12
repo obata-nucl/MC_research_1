47a2691ec3dd9e8c75a6d0f7dfb885556397dd5a
Obata Yutaka <obata0904@outlook.jp>
2026-01-12 14:38:18 +0900

    feat/input: update NN inputs to include proton boson number

--- src/physics.py
+++ src/physics.py
@@ -6,16 +6,18 @@ def IBM2_PES(params: torch.Tensor, n_pi: torch.Tensor, n_nu: torch.Tensor, beta_
     """ Compute IBM2 potential energy surface (PES) """
     eps = params[:, 0].unsqueeze(1)
     kappa = params[:, 1].unsqueeze(1)
-    chi_n = params[:, 2].unsqueeze(1)
-    beta_b = beta_f*2.5
+    chi_pi = params[:, 2].unsqueeze(1)
+    chi_nu = params[:, 3].unsqueeze(1)
+    beta_b = beta_f*3.0
 
     beta2 = beta_b**2
-    A = 1/(1 + beta2)
-    SQRT_2_OVER_7 = torch.tensor(math.sqrt(2.0/7.0), dtype=params.dtype, device=params.device)
+    deno1 = 1 + beta2
+    deno2 = deno1**2
+    SQRT_2_7 = 0.5345224838248488
 
-    term_1 = eps*(n_pi + n_nu)*beta2*A
-    term_2 = n_pi*n_nu*kappa*beta2*A*A*(
-        4.0 - 2.0*SQRT_2_OVER_7*(chi_n - 0.5)*beta_b - chi_n*beta2/7.0
-    )
+    term_1 = eps*(n_pi + n_nu)*beta2/deno1
+    term_2 = n_pi*n_nu*kappa*beta2*(
+        4.0 - 2.0*SQRT_2_7*(chi_nu + chi_pi)*beta_b + 2.0*chi_nu*chi_pi*beta2/7.0
+    )/deno2
 
     return term_1 + term_2
